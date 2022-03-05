9b2ad510bc74163ed2508c10020457fdcf281939
manatsawi-idev <manatsawi.git@gmail.com>
2022-03-06 02:19:47 +0700

    update: function create random question

--- app/redux/random-question/__test__/utils.test.ts
+++ app/redux/random-question/__test__/utils.test.ts
@@ -3,10 +3,26 @@ import * as utils from '../utils';
 import dataQuestions from '../../../res/data';
 
 describe('Redux random question utils createChoices', () => {
-  test('should return shuffled array of choice correctly', () => {
-    const choicesWithAns = ['A', 'B', 'C', 'D'];
-    const allChoice = ['A', 'B', 'C', 'D', 'E', 'F'];
-    const choices = utils.createChoices(choicesWithAns, allChoice);
+  const allChoice: string[] = ['A', 'B', 'C', 'D', 'E', 'F'];
+  test('should return empty array correctly. ans = empty string', () => {
+    const ans: string = '';
+    const choices = utils.createChoices(ans, allChoice);
+    expect(choices.length).toEqual(0);
+  });
+  test('should return return empty array correctly. base choices = empty array', () => {
+    const ans: string = 'A';
+    const choices = utils.createChoices(ans, []);
+    expect(choices.length).toEqual(0);
+  });
+  test('should return shuffled array of choice correctly ans = A', () => {
+    const ans: string = 'A';
+    const choices = utils.createChoices(ans, allChoice);
+    expect(choices.length).toEqual(4);
+    expect(choices).not.toEqual(allChoice.slice(0, 4));
+  });
+  test('should return shuffled array of choice correctly ans = B', () => {
+    const ans: string = 'B';
+    const choices = utils.createChoices(ans, allChoice);
     expect(choices.length).toEqual(4);
     expect(choices).not.toEqual(allChoice.slice(0, 4));
   });
@@ -15,8 +31,15 @@ describe('Redux random question utils createChoices', () => {
 describe('Redux random question utils createRandomQuestion', () => {
   test('should return shuffled array of question correctly', () => {
     const question = utils.createRandomQuestion();
+    const question2 = utils.createRandomQuestion();
+    const question3 = utils.createRandomQuestion();
     expect(question.length).toEqual(20);
+    expect(question2.length).toEqual(20);
+    expect(question3.length).toEqual(20);
     expect(question).not.toEqual(dataQuestions.slice(0, 20));
+    expect(question2).not.toEqual(dataQuestions.slice(0, 20));
+    expect(question3).not.toEqual(dataQuestions.slice(0, 20));
+    console.log(question, 'question-set-1');
   });
   test('should return shuffled array of choices correctly', () => {
     const questions = utils.createRandomQuestion();
--- app/redux/random-question/utils.ts
+++ app/redux/random-question/utils.ts
@@ -4,13 +4,14 @@ import isEqual from 'lodash/isEqual';
 import dataQuestions from '../../res/data';
 import * as I from '../../Interface';
 
-export const createChoices = (listChoice: string[], listBaseChoice: string[]): string[] => {
-  if (!Array.isArray(listChoice) || !Array.isArray(listBaseChoice)) {
+export const createChoices = (ans: string, listBaseChoice: string[]): string[] => {
+  if (!ans || !Array.isArray(listBaseChoice) || (Array.isArray(listBaseChoice) && !listBaseChoice.length)) {
     return [];
   }
-  const choices = shuffle(listChoice);
+  const baseChoices = shuffle(listBaseChoice.filter(a => a !== ans)).slice(0, 3);
+  const choices = shuffle([ans, ...baseChoices]);
   if (isEqual(choices, listBaseChoice.slice(0, 4))) {
-    return createChoices(choices, listBaseChoice);
+    return createChoices(ans, listBaseChoice);
   }
   return choices;
 };
@@ -19,8 +20,7 @@ export const createRandomQuestion = (): I.questions[] => {
   const keysBaseQuestion = keyBy(dataQuestions, 'id');
   const shuffledQuestion = shuffle(dataQuestions).slice(0, 20);
   const shuffledAnswer = shuffledQuestion.map(item => {
-    const choices = item.choices.filter(a => a !== item.ans).slice(0, 3);
-    const shuffledChoice = createChoices([item.ans, ...choices], keysBaseQuestion[item.id].choices);
+    const shuffledChoice = createChoices(item.ans, keysBaseQuestion[item.id].choices);
     const question = {
       ...item,
       choices: shuffledChoice,
